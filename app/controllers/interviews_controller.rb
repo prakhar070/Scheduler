4f535103dffcddbb5c0621328943edc4c8139833
prakhar070 <bansalprakhar.2266@gmail.com>
2020-05-22 14:26:27 +0530

    able to create datetime object at the backend

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -3,10 +3,15 @@ class InterviewsController < ApplicationController
         @details = hash_for_index
     end
 
-
-
     def create
-
+        @dt = DateTime.new(
+            params[:year].to_i, 
+            params[:month].to_i, 
+            params[:day].to_i, 
+            params[:time].split(":")[0].to_i,
+            params[:time].split(":")[1].to_i
+        )
+        print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$", @dt)
     end
 
     private
@@ -15,12 +20,10 @@ class InterviewsController < ApplicationController
         if params[:monthyear]
             details[:month] = params[:monthyear].split("-")[1].to_i
             details[:year] = params[:monthyear].split("-")[0].to_i
-            details[:month_name] = Date::MONTHNAMES[details[:month]]
             details[:days] = Time.days_in_month(details[:month], details[:year])
         else
             details[:month] = Date.today.month
             details[:year] = Date.today.year
-            details[:month_name] = Date::MONTHNAMES[details[:month]]
             details[:days] = Time.days_in_month(details[:month], details[:year])
         end
         return details
